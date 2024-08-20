6d98e9c31e1f65b8f303f0e1f5faf1a2db5bda5b
haitang000 <haitang.36038@qq.com>
2024-08-20 08:46:28 +0800

    ✨ 增加底栏

--- index.js
+++ index.js
@@ -86,3 +86,29 @@ if (month == 12 && day == 5) {
     Titleh1.innerText = "🎉 今天是夏彦的生日 12.5 🎉"; //标题
     Titlep.innerText = "祝  ☀比冬日暖阳还要耀眼的你☀  快乐安康 万事顺遂" //描述
 }
+function siteTime(){
+    window.setTimeout("siteTime()", 1000);
+    var seconds = 1000;
+    var minutes = seconds * 60;
+    var hours = minutes * 60;
+    var days = hours * 24;
+    var years = days * 365;
+    var today = new Date();
+    var todayYear = today.getFullYear();
+    var todayMonth = today.getMonth()+1;
+    var todayDate = today.getDate();
+    var todayHour = today.getHours();
+    var todayMinute = today.getMinutes();
+    var todaySecond = today.getSeconds();
+    // Date.UTC() -- 返回date对象距世界标准时间(UTC)1970年1月1日午夜之间的毫秒数(时间戳)
+    var t1 = Date.UTC(2024,08,15,13,19,17); //北京时间2017-09-16 00:00:00创建网站的时间
+    var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
+    var diff = t2-t1;
+    var diffYears = Math.floor(diff/years);
+    var diffDays = Math.floor((diff/days)-diffYears*365);
+    var diffHours = Math.floor((diff-(diffYears*365+diffDays)*days)/hours);
+    var diffMinutes = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours)/minutes);
+    var diffSeconds = Math.floor((diff-(diffYears*365+diffDays)*days-diffHours*hours-diffMinutes*minutes)/seconds);
+    document.getElementById("sitetime").innerHTML="本站已运行"+diffYears+"年"+diffDays+"天"+diffHours+"时"+diffMinutes+"分钟"+diffSeconds+"秒";
+}
+siteTime();
