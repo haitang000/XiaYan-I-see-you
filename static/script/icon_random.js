2ce6d1edb4db294098111d2a185e1cfee565d649
haitang000 <haitang.36038@qq.com>
2024-08-27 10:58:54 +0800

    👌 优化Icon随机逻辑

--- static/script/icon_random.js
+++ static/script/icon_random.js
@@ -1,7 +1,36 @@
-window.onload = randomErrorIcon;
+/*window.onload = randomIcon;
 
-function randomErrorIcon (){
-    var ErrorIcon = new Array('image/icon/Thinking.png','image/icon/Cry.png','image/icon/Alone.png')
-    var randomNum = Math.floor((Math.random() * ErrorIcon.length));
-    document.getElementById("error-icon").src = ErrorIcon[randomNum];
+function randomIcon (){
+    //图片路径
+    var ErrorIcon = new Array('image/icon/Thinking.png','image/icon/Cry.png','image/icon/Cry2.png','image/icon/Alone.png')
+    var MaintenanceIcon = new Array('image/icon/Study.png','image/icon/Thinking.png','image/icon/PlayWithPhone.png')
+    //随机数生成
+    var randomNumError = Math.floor((Math.random() * ErrorIcon.length));
+    var randomNumMaintenance = Math.floor((Math.random() * MaintenanceIcon.length));
+    //替换图片
+    document.getElementById("error-icon").src = ErrorIcon[randomNumError];
+    document.getElementById("maintenance-icon").src = MaintenanceIcon[randomNumMaintenance];
+}*/
+
+window.onload = function() {
+    randomIcon('error-icon', [
+        'image/icon/Thinking.png',
+        'image/icon/Cry.png',
+        'image/icon/Cry2.png',
+        'image/icon/Alone.png'
+    ]);
+
+    randomIcon('maintenance-icon', [
+        'image/icon/Study.png',
+        'image/icon/Thinking.png',
+        'image/icon/PlayWithPhone.png'
+    ]);
+};
+
+function randomIcon(elementId, iconArray) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const randomIndex = Math.floor(Math.random() * iconArray.length);
+        element.src = iconArray[randomIndex];
+    }
 }
