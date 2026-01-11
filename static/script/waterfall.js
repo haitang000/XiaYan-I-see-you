9b7be377a06aa0d6fe11158ad310eb5a8f153246
haitang000 <3603866430@qq.com>
2026-01-11 11:47:42 +0800

    ⚡️ 优化项目

--- static/script/waterfall.js
+++ static/script/waterfall.js
@@ -237,27 +237,36 @@ function cal() {
 }
 
 // 设置每张图片的位置
+// 设置每张图片的位置
 function setPositions() {
     // 获取列数和间隙
     let info = cal();
     // 该数组的长度为列数，每一项表示该列的下一个图片的纵坐标
-    let next_tops = new Array(info.columns);
-    // 将数组的每一项填充为0
-    next_tops.fill(0);
-    for (let i = 0; i < container.children.length; i++) {
-        let img = container.children[i];
+    let next_tops = new Array(info.columns).fill(0);
+
+    const imgs = Array.from(container.children);
+    // Batch READ: Get all heights first to avoid layout thrashing in loop
+    const heights = imgs.map(img => img.offsetHeight);
+
+    // Batch WRITE: Apply styles
+    imgs.forEach((img, i) => {
         // 找到next_tops中的最小值作为当前图片的纵坐标
-        let min_top = Math.min.apply(null, next_tops);
+        let min_top = Math.min(...next_tops);
         img.style.top = min_top + 'px';
+
         // 重新设置数组这一项的下一个top值
         let index = next_tops.indexOf(min_top); //得到使用的是第几列的top值
-        next_tops[index] += img.height + info.space;
+
+        // Use cached height
+        next_tops[index] += heights[i] + info.space;
+
         // 计算横坐标
         let left = (index + 1) * info.space + index * img_width;
         img.style.left = left + 'px';
-    }
+    });
+
     // 得到next_tops中的最大值
-    let max = Math.max.apply(null, next_tops);
+    let max = Math.max(...next_tops);
     // 设置容器的高度
     container.style.height = max + 'px';
 }
